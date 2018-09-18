5844b7defce5ea4bf91b654fa7d8f154d213a35c
katsudonik <23866351+katsudonik@users.noreply.github.com>
2018-09-18 15:33:35 +0900

    .

--- calc_soil_locations/management/commands/shot.py
+++ calc_soil_locations/management/commands/shot.py
@@ -5,6 +5,10 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 from modules.screenshot import shot_jpg
 
+from calc_soil_locations.models import SoilDisposalTask
+import string
+import random
+
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
@@ -13,10 +17,34 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print(options['task_id'])
         for task_id in options['task_id']:
+            img_path = settings.SOIL_LOCATIONS['default']['image_dir'] + self.random_character(10)
+            
             shot_jpg(
                 settings.SOIL_LOCATIONS['default']['url'] + '?task_id=' + str(task_id),
                 settings.SOIL_LOCATIONS['default']['width'],
                 settings.SOIL_LOCATIONS['default']['height'],
                 settings.SOIL_LOCATIONS['default']['quality'],
-                settings.SOIL_LOCATIONS['default']['image_dir'] + str(task_id)
+                img_path,
+                settings.PHANTOM_JS['path']
             )
+        
+            task = self.fetch_task(task_id)
+            task.file_name = img_path
+            task.updated_at = timezone.now()
+            task.save()
+            
+    def fetch_task(self, task_id):
+        task = SoilDisposalTask.objects.filter(id=task_id).first()
+        if task.at_time_field_latitude is None:
+            raise UnboundLocalError
+        if task.at_time_field_longitude is None:
+            raise UnboundLocalError
+        if task.at_time_soil_disposal_site_latitude is None:
+            raise UnboundLocalError
+        if task.at_time_soil_disposal_site_longitude is None:
+            raise UnboundLocalError
+        return task
+    
+    def random_character(self, n):
+        c = string.ascii_lowercase + string.ascii_uppercase + string.digits
+        return ''.join([random.choice(c) for i in range(n)])
