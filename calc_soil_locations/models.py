5fa6b3dbb9c97005847ac6ceb6e57a2d1e5d58e4
katsudonik <23866351+katsudonik@users.noreply.github.com>
2018-09-18 15:33:39 +0900

    .

--- calc_soil_locations/models.py
+++ calc_soil_locations/models.py
@@ -6,13 +6,15 @@ class SoilDisposalTask(models.Model):
     at_time_field_longitude  = models.DecimalField(max_digits=11, decimal_places=8)
     at_time_soil_disposal_site_latitude  = models.DecimalField(max_digits=10, decimal_places=8)
     at_time_soil_disposal_site_longitude  = models.DecimalField(max_digits=11, decimal_places=8)
-    image_path  = models.TextField()
+    file_name  = models.TextField()
     mileage = models.DecimalField(max_digits=8, decimal_places=0)
     co2_emissions  = models.DecimalField(max_digits=13, decimal_places=3)
     driving_time_second = models.BigIntegerField()
     arrived_to_site_flg  = models.BooleanField()
     updated_at = models.DateTimeField(default=timezone.now)
-
+    start_time = models.TimeField(default=timezone.now)
+    end_time = models.TimeField(default=timezone.now)
+    
     class Meta:
         db_table = 'soil_disposal_tasks'
 
@@ -21,5 +23,5 @@ class SoilDisposalTask(models.Model):
         self.save()
 
     def __str__(self):
-        return self.image_path
+        return self.file_name
 
